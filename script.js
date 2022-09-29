7e3c229bfa641359b4857ad6c1ca84d1816be130
ShivaniVora <shivanivora797@icloud.com>
2022-09-29 19:57:44 -0400

    layout

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-import "node-fetch";
+import fetch from "node-fetch";
 
+fetch("https://pokeapi.co/api/v2/pokemon/1")
+    .then((resp) => resp.json())
+    .then((data) => showName(data.name));
+
+showName = (dn => {
+    console.log('runs')
+    const nameBox = document.getElementById('name');
+    nameBox.textContent = dn;
+})
