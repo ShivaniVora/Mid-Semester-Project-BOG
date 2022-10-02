5f342152a9f4ef002c31a046bb9d073987b0e4a5
ShivaniVora <shivanivora797@icloud.com>
2022-10-02 19:23:44 -0400

    starting fetching code

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-fetch("https://pokeapi.co/api/v2/pokemon/1")
+id = 1
+const rightArrow = document.getElementById("arrowr");
+
+rightArrow.addEventListener("click", (e) => {
+    id = id+1
+})
+
+
+var temp = "https://pokeapi.co/api/v2/pokemon/NUM_HERE";
+var newUrl = temp.replace("NUM_HERE", id)
+
+fetch(newUrl)
     .then((resp) => resp.json())
-    .then((data) => showName(data.name));
+    .then((data) => allfunctions(data))
+
+allfunctions = (data => {
+    showName(data.name);
+    getImage(data.id);
+})
+
 
 showName = (dn => {
-    console.log('runs')
     const nameBox = document.getElementById('name');
     nameBox.textContent = dn;
+})
+
+
+getImage = (im => {
+    var ul = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/NUM_HERE.png"
+    var url = ul.replace("NUM_HERE", im);
+    const imageLink = document.getElementById('image');
+    imageLink.src = url;
 })
