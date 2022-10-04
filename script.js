68c0de32e2464627677d02c99f8d7165783324f4
ShivaniVora <shivanivora797@icloud.com>
2022-10-04 00:18:44 -0400

    finished image and name reaction to arrow button click

--- script.js
+++ script.js
@@ -1,33 +1,81 @@
-id = 1
+id = 1;
 const rightArrow = document.getElementById("arrowr");
+const leftArrow = document.getElementById("arrowl");
+const moves = document.getElementById("m");
+const info = document.getElementById("i");
+iM = 1;
+changeImage(id, iM);
+
+
+info.addEventListener("click", (e) => {
+    iM = 1;
+    changeImage(id, iM);
+})
+
+moves.addEventListener("click", (e) => {
+    iM = 2;
+    changeImage(id, iM);
+})
+
+leftArrow.addEventListener("click", (e) => {
+    if(id-1 >0) {
+        id = id-1;
+        changeImage(id, iM);
+    }
+})
 
 rightArrow.addEventListener("click", (e) => {
-    id = id+1
-})
+  id = id + 1;
+  changeImage(id)
+});
 
+var text = ""
+var moveCount = 0
+function getMoves(data) {
+    data.forEach(createString)
+    moveCount++;
+}
 
-var temp = "https://pokeapi.co/api/v2/pokemon/NUM_HERE";
-var newUrl = temp.replace("NUM_HERE", id)
+function createString(movement) {
+    text = text + movement.moveCount.move.name + "\n";
+}
 
-fetch(newUrl)
+function changeImage(id, iM) {
+    var temp = "https://pokeapi.co/api/v2/pokemon/NUM_HERE";
+  var newUrl = temp.replace("NUM_HERE", id);
+
+  fetch(newUrl)
     .then((resp) => resp.json())
-    .then((data) => allfunctions(data))
+    .then((data) => allfunctions(data));
 
-allfunctions = (data => {
+  allfunctions = (data) => {
     showName(data.name);
     getImage(data.id);
-})
+    infoAndMoves(data);
+  };
 
-
-showName = (dn => {
-    const nameBox = document.getElementById('name');
+  showName = (dn) => {
+    const nameBox = document.getElementById("name");
     nameBox.textContent = dn;
-})
+  };
 
-
-getImage = (im => {
-    var ul = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/NUM_HERE.png"
+  getImage = (im) => {
+    var ul =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/NUM_HERE.png";
     var url = ul.replace("NUM_HERE", im);
-    const imageLink = document.getElementById('image');
+    const imageLink = document.getElementById("image");
     imageLink.src = url;
-})
+  };
+
+  infoAndMoves = (data) => {
+    const infoMovesBox = document.getElementById("info-moves-box")
+    if(iM === 1) {
+        infoMovesBox.textContent = data.name
+    }
+    else {
+        // getMoves(data.moves);
+        infoMovesBox.textContent = "moves here";
+    }
+    
+  };
+}
